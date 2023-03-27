61ce3dd1f8b39518ea9cf288fe44746197c245e9
Adam Fields <afields@6river.com>
2023-03-27 00:47:17 -0400

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://astro-yt3.pages.dev',
+  integrations: [mdx()],
+  output: "server",
+  adapter: cloudflare()
 });
